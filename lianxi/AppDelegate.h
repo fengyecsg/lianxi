c6291e1218a4ccd809d6db64e4f57d8ff24057ac
fengye <850379974@qq.com>
2014-06-17 15:57:57 +0800

    Initial Commit

---
+++ lianxi/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  lianxi
+//
+//  Created by csg on 14-6-17.
+//  Copyright (c) 2014年 蓝鸥科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ lianxi/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  lianxi
+//
+//  Created by csg on 14-6-17.
+//  Copyright (c) 2014年 蓝鸥科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
